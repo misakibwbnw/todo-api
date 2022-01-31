4a6ae0561eeb63ce1a0d675acd6ce9562a9278a9
misakibwbnw <mmys.a.fk@gmail.com>
2022-01-31 23:58:38 +0900

    api fix

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # todo-api
-参考
-https://sbfl.net/blog/2018/08/25/nodejs-express-webapi/
-
+## 開発
 dev
 ```
 npm run dev
 ```
+
+デバッグ時のcurlコマンド例
+```
+curl -X POST http://localhost:8080/api/v1/list -H "Content-type: application/json" -d '{ "title" : "tanaka" }'
+```
+## 参考
+todoアプリ生成
+https://sbfl.net/blog/2018/08/25/nodejs-express-webapi/
+id生成
+https://qiita.com/kawasima/items/6b0f47a60c9cb5ffb5c4
--- index.js
+++ index.js
@@ -1,31 +1,71 @@
 // expressモジュールを読み込む
-const express = require('express');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 // expressアプリを生成する
-const app = express();
-
-// ルート（http://localhost/）にアクセスしてきたときに「Hello」を返す
-// app.get('/', (req, res) => res.send('Hello'));
+const app = express()
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 200
+}))
+app.use(bodyParser.json());
+const todoList = []
+let todoItemIds = 0
 
 app.get('/api/v1/list', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    res.json(todoList)
+})
+
+app.post('/api/v1/list', (req, res) => {
+    todoItemIds = todoItemIds + 1
+    const todoItem = {
+        id: todoItemIds,
+        title: req.body.title,
+        checked: false
+    }
+    todoList.push(todoItem)
+
+    res.json(todoItem)
+})
+
+app.delete('/api/v1/list/:id', (req, res) => {
+    const index = todoList.findIndex((item) => item.id === req.params.id)
+    console.log(req)
+
+    // 値の更新
+    if (index > 0) {
+        const deleted = todoList.splice(index, 1)
+    }
+
+    res.sendStatus(200)
+})
+
+app.put('/api/v1/list/:id', (req, res) => {
+    const index = todoList.findIndex((item) => item.id === req.params.id)
     
-    // クライアントに送るJSONデータ
-    const todoList = [
-        { title: 'JavaScriptを勉強する', done: true },
-        { title: 'Node.jsを勉強する', done: false },
-        { title: 'Web APIを作る', done: false }
-    ];
 
-    // JSONを送信する
-    res.json(todoList);
-});
+    // 値の更新
+    if (index > 0) {
+        const item = todoList[index]
+        Object.keys(req).forEach((key) => {
+            item[key] = req[key]
+        })
+    }
+    // const todoItem = {
+    //     id: todoItemIds + 1,
+    //     title: req.body.title,
+    //     checked: false
+    // }
+    // todoList.push(todoItem)
 
+    res.json(todoList)
+})
 
 // ポート8080でサーバを立てる
-app.listen(8080, () => console.log('Listening on port 8080'));
+app.listen(8080, () => console.log('Listening on port 8080'))
 
 
 
